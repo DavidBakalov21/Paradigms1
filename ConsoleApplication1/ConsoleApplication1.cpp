25bd00f59c5a57f85fd64cb49fb34a8f3050842d
DavidBakalov21 <davidbakalov33@gmail.com>
2023-09-07 19:55:06 +0300

    dynamic array

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,8 +1,22 @@
-﻿
+﻿#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;  // Dereferencing to get the value of 'a'
+    *a = *b;    // Dereferencing to assign the value of 'b' to 'a'
+    *b = temp;
+}
 int main() {
     /*
+    int x = 5;
+     int y = 10;
+     int* yP = &y;
+         int* xP = &x;
+     swap(xP, yP);
+     printf("After swapping: x = %d, y = %d\n", x, y);
+
+
     int r;
     char str[100];
     printf("Hello, World!\n");
@@ -15,10 +29,19 @@ int main() {
      //   printf("The string is 'ff'.\n");
         // You can place additional code here to do something when the string equals "ff"
    // }
-   */
+    */
+       
     bool loop = true;
     
-    char text[900]="";
+    //char text[900]="";
+
+     int capacity = 30;  // initial capacity
+    char* text = (char*)malloc(capacity * sizeof(char));
+    if (!text) {
+        printf("Failed to allocate memory.\n");
+        return 1;
+    }
+    text[0] = '\0';
     printf("1-Append in the end, 2-Start new line, 3-Save to file, 4-load from file, 5-print to console, 6-Insert text by indexes, 7-search, 8-clear\n");
     while (loop == true) {
         char str;
@@ -30,13 +53,25 @@ int main() {
         case '1':
         {
             printf("Append in the end\n");
-            char appendText[200];
+            char appendText[20];
+            
+          
             fgets(appendText, sizeof(appendText), stdin); // reads the newline too, so you may want to remove it
             size_t len = strlen(appendText);
             if (len > 0 && appendText[len - 1] == '\n') {
                 appendText[len - 1] = '\0'; // replace newline with null terminator
             }
-            strcat_s(text, sizeof(text), appendText);
+         
+            if (strlen(text) + len + 1 > capacity) {
+                capacity += len + 1;  // increase capacity
+                text = (char*)realloc(text, capacity * sizeof(char));  // resize the array
+                if (!text) {
+                    printf("Failed to allocate memory for text.\n");
+                    return 1;
+                }
+            }
+
+            strcat_s(text, capacity, appendText);
             printf("The entered text is: %s\n", text);
             break;
         }
@@ -71,6 +106,6 @@ int main() {
         }
         
     }
-    
+   
     return 0;
 }
